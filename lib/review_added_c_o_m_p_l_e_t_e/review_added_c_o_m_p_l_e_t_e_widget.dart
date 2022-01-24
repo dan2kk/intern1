9f4cecbf2f978417530f78d4c8be0d10b6c7dd6a
sph0514 <luckyspda@naver.com>
2022-01-24 12:04:03 +0900

    디자인 수정

--- lib/review_added_c_o_m_p_l_e_t_e/review_added_c_o_m_p_l_e_t_e_widget.dart
+++ lib/review_added_c_o_m_p_l_e_t_e/review_added_c_o_m_p_l_e_t_e_widget.dart
@@ -20,59 +20,97 @@ class _ReviewAddedCOMPLETEWidgetState extends State<ReviewAddedCOMPLETEWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       key: scaffoldKey,
-      backgroundColor: Color(0xFF5C6EBE),
+      backgroundColor: Color(0xFFF5F5F5),
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsetsDirectional.fromSTEB(0, 200, 0, 0),
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.4,
-            decoration: BoxDecoration(
-              color: Color(0xFFEEEEEE),
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
-                  child: Icon(
-                    Icons.check_circle,
-                    color: Color(0xFF4B39EF),
-                    size: 70,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.5,
+              decoration: BoxDecoration(
+                color: Color(0xFFF5F5F5),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.check_circle,
+                        color: Color(0xFF21B6FF),
+                        size: 70,
+                      ),
+                    ],
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
-                  child: Container(
+                  Container(
                     width: MediaQuery.of(context).size.width,
-                    height: 100,
+                    height: MediaQuery.of(context).size.height * 0.2,
                     decoration: BoxDecoration(
-                      color: Color(0xFFEEEEEE),
+                      color: Color(0xFFF5F5F5),
                     ),
                     child: Column(
                       mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Row(
                           mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Container(
-                              width: MediaQuery.of(context).size.width,
-                              height: 100,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFEEEEEE),
+                            Text(
+                              '리뷰 작성 완료',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontFamily: 'tway_air medium',
+                                fontWeight: FontWeight.w500,
+                                fontSize: 25,
                               ),
-                              child: Padding(
-                                padding:
-                                    EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
-                                child: Text(
-                                  '작성 하신 리뷰가 등록되었습니다!',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: 18,
-                                  ),
-                                ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontFamily: 'tway_air medium',
+                                color: Color(0xFF5C6EBE),
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18,
+                              ),
+                            ),
+                            Text(
+                              '포인트 적립',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontFamily: 'tway_air medium',
+                                color: Color(0xFF5C6EBE),
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '소중한 리뷰 감사합니다',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontFamily: 'tway_air medium',
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18,
                               ),
                             ),
                           ],
@@ -80,18 +118,15 @@ class _ReviewAddedCOMPLETEWidgetState extends State<ReviewAddedCOMPLETEWidget> {
                       ],
                     ),
                   ),
-                ),
-                Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
-                      child: Row(
+                  Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Row(
                         mainAxisSize: MainAxisSize.max,
                         children: [
                           Padding(
                             padding:
-                                EdgeInsetsDirectional.fromSTEB(50, 0, 0, 0),
+                            EdgeInsetsDirectional.fromSTEB(50, 0, 0, 0),
                             child: Container(
                               width: 120,
                               height: 60,
@@ -111,10 +146,11 @@ class _ReviewAddedCOMPLETEWidgetState extends State<ReviewAddedCOMPLETEWidget> {
                                 text: '메인 화면',
                                 options: FFButtonOptions(
                                   height: 30,
-                                  color: Color(0xFF4B39EF),
+                                  color: Color(0xFF21B6FF),
                                   textStyle: TextStyle(
+                                    fontFamily: 'tway_air medium',
                                     color: Colors.white,
-                                    fontWeight: FontWeight.w600,
+                                    fontWeight: FontWeight.w500,
                                     fontSize: 17,
                                   ),
                                   borderSide: BorderSide(
@@ -128,7 +164,7 @@ class _ReviewAddedCOMPLETEWidgetState extends State<ReviewAddedCOMPLETEWidget> {
                           ),
                           Padding(
                             padding:
-                                EdgeInsetsDirectional.fromSTEB(35, 0, 0, 0),
+                            EdgeInsetsDirectional.fromSTEB(35, 0, 0, 0),
                             child: Container(
                               width: 120,
                               height: 60,
@@ -148,10 +184,11 @@ class _ReviewAddedCOMPLETEWidgetState extends State<ReviewAddedCOMPLETEWidget> {
                                 text: '마이 페이지',
                                 options: FFButtonOptions(
                                   height: 30,
-                                  color: Color(0xFF4B39EF),
+                                  color: Color(0xFF21B6FF),
                                   textStyle: TextStyle(
+                                    fontFamily: 'tway_air medium',
                                     color: Colors.white,
-                                    fontWeight: FontWeight.w600,
+                                    fontWeight: FontWeight.w500,
                                     fontSize: 17,
                                   ),
                                   borderSide: BorderSide(
@@ -165,12 +202,12 @@ class _ReviewAddedCOMPLETEWidgetState extends State<ReviewAddedCOMPLETEWidget> {
                           ),
                         ],
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
